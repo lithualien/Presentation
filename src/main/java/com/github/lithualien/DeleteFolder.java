93c85b9c2fcb93a22839c1a9e1350635aaf1bd9d
lithualien <45800655+lithualien@users.noreply.github.com>
2019-04-17 01:01:06 +0300

    done for today

--- src/main/java/com/github/lithualien/DeleteFolder.java
+++ src/main/java/com/github/lithualien/DeleteFolder.java
@@ -3,7 +3,14 @@ package com.github.lithualien;
 import java.io.File;
 
 public class DeleteFolder {
-    public void deleteFolder(File file) {
+
+    public DeleteFolder(File file) {
+        deleteFolder(file);
+        file.delete();
+        System.out.println("The folder was successfully deleted.");
+    }
+
+    private void deleteFolder(File file) {
         try {
             File files[] = file.listFiles();
             for (File temp : files) {
